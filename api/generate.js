36ad1c5611a4d074b32e369b275109fcf5bff9a0
proskingstas1-dol <proskingstas1@gmail.com>
2026-09-22 04:54:53 +0000

    Trial 92 Bknd stas imp

---
+++ api/generate.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method not allowed'
+    });
+  }
+
+  return res.status(200).json({
+    ok: true,
+    message: 'Generation backend is connected.'
+  });
+}
